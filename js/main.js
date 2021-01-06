b3efecf7914c8609b6badac97b6a1558649fd7a0
Piotr Stachowicz <ptr.stachowicz@gmail.com>
2021-01-06 15:23:38 +0100

    fix nav scroll to section

--- js/main.js
+++ js/main.js
@@ -12,11 +12,11 @@ navAllLinks.forEach(function (navLink) {
 
         e.preventDefault();
         const element = document.querySelector("[data-sectionin=" + this.dataset.section + "]");
-        const offset = 80;
+        const offset = 100;
         const bodyRect = document.body.getBoundingClientRect().top;
         const elementRect = element.getBoundingClientRect().top;
         const elementPosition = elementRect - bodyRect;
-        const offsetPosition = elementPosition + offset;
+        const offsetPosition = elementPosition - offset;
 
         window.scrollTo({
             top: offsetPosition,

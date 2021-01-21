75998c8cc437cb7e331642c11acfa5f4e58fc8a4
Piotr Stachowicz <ptr.stachowicz@gmail.com>
2021-01-21 01:35:51 +0100

    add eslint + config

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-"use strict"
+'use strict';
 
 const hambPop = document.querySelector('.navBurger');
 const hambButton = document.querySelector('.navBurgerBox__btn');
@@ -11,78 +11,72 @@ const navAllBurgerLinks = document.querySelectorAll('.navBurger__link');
 const allOffers = document.querySelectorAll('.squareOffers__offer');
 
 navAllLinks.forEach((navLink) => {
-    navLink.addEventListener('click', function (e) {
+  navLink.addEventListener('click', function (e) {
+    e.preventDefault();
+    const element = document.querySelector('[data-sectionin=' + this.dataset.section + ']');
+    const offset = 100;
+    const bodyRect = document.body.getBoundingClientRect().top;
+    const elementRect = element.getBoundingClientRect().top;
+    const elementPosition = elementRect - bodyRect;
+    const offsetPosition = elementPosition - offset;
 
-        e.preventDefault();
-        const element = document.querySelector("[data-sectionin=" + this.dataset.section + "]");
-        const offset = 100;
-        const bodyRect = document.body.getBoundingClientRect().top;
-        const elementRect = element.getBoundingClientRect().top;
-        const elementPosition = elementRect - bodyRect;
-        const offsetPosition = elementPosition - offset;
-
-        window.scroll({
-            top: offsetPosition,
-            behavior: 'smooth'
-        });
-    })
-})
+    window.scroll({
+      top: offsetPosition,
+      behavior: 'smooth'
+    });
+  });
+});
 
 navAllBurgerLinks.forEach((navBurgerLink) => {
-    navBurgerLink.addEventListener('click', function (e) {
-
-        e.preventDefault();
-        hambActiveFlag = !hambActiveFlag;
-        hambButton.classList.toggle('navBurgerBox__btn--active');
-        hambButton.classList.toggle('navBurgerBox__btn--notActive');
-        hambPop.classList.toggle('navBurger--show');
-        hambPop.classList.toggle('navBurger--hide');
-
-        const goToSection = "[data-sectionin=" + this.dataset.section + "]";
-        document.querySelector(goToSection).scrollIntoView({
-            behavior: 'smooth'
-        });
-
-    })
-})
-
-boxBurger.addEventListener('click', (e) => {
-
+  navBurgerLink.addEventListener('click', function (e) {
     e.preventDefault();
+    hambActiveFlag = !hambActiveFlag;
     hambButton.classList.toggle('navBurgerBox__btn--active');
     hambButton.classList.toggle('navBurgerBox__btn--notActive');
     hambPop.classList.toggle('navBurger--show');
     hambPop.classList.toggle('navBurger--hide');
-    hambPop.style.display = 'block';
-})
+
+    const goToSection = '[data-sectionin=' + this.dataset.section + ']';
+    document.querySelector(goToSection).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
+
+boxBurger.addEventListener('click', (e) => {
+  e.preventDefault();
+  hambButton.classList.toggle('navBurgerBox__btn--active');
+  hambButton.classList.toggle('navBurgerBox__btn--notActive');
+  hambPop.classList.toggle('navBurger--show');
+  hambPop.classList.toggle('navBurger--hide');
+  hambPop.style.display = 'block';
+});
 
 window.addEventListener('scroll', () => {
+  var isSafari = window.safari !== undefined;
+  let bodyelem = '';
 
-    var isSafari = window.safari !== undefined;
-    let bodyelem = '';
+  if (isSafari) {
+    bodyelem = document.querySelector('body');
+  } else {
+    bodyelem = window;
+  }
 
-    if (isSafari) {
-        bodyelem = document.querySelector('body')
-    } else {
-        bodyelem = window;
-    }
+  const windowHeight = bodyelem.innerHeight;
+  const scrollValue = bodyelem.scrollY;
+  const nav = document.querySelector('nav');
+  const navList = document.querySelector('.nav__list');
 
-    const windowHeight = bodyelem.innerHeight;
-    const scrollValue = bodyelem.scrollY;
-    const nav = document.querySelector('nav');
-    const navList = document.querySelector('.nav__list');
-
-    if ((scrollValue < 20)) {
-        nav.classList.remove('nav--transBgc');
-        nav.classList.add('nav--basic');
-        navList.classList.add('nav__list--onTop');
-
-    } else if ((scrollValue < windowHeight)) {
-        nav.classList.add('nav--transBgc');
-        nav.classList.remove('nav--basic');
-        navList.classList.remove('nav__list--onTop');
-    }
-})
+  if (scrollValue < 20) {
+    nav.classList.remove('nav--transBgc');
+    nav.classList.add('nav--basic');
+    navList.classList.add('nav__list--onTop');
+  } else if (scrollValue < windowHeight) {
+    nav.classList.add('nav--transBgc');
+    nav.classList.remove('nav--basic');
+    navList.classList.remove('nav__list--onTop');
+  }
+});
 
 // const aboutMoreInfoBtn = document.querySelector('.columnTexts__btn--aboutMoreInfo');
 // const aboutMoreInfo = document.querySelector('.columnTexts__moreInfo');
@@ -90,56 +84,55 @@ window.addEventListener('scroll', () => {
 //     aboutMoreInfo.classList.toggle('columnTexts__moreInfo--hide');
 // })
 
-$(document).ready(function() {
-    $('.columnTexts__btn--hideStart').click(function() {
-        $('.columnTexts__moreInfo--hideStart').slideToggle("slow");
-    });
+$(document).ready(function () {
+  $('.columnTexts__btn--hideStart').click(function () {
+    $('.columnTexts__moreInfo--hideStart').slideToggle('slow');
+  });
 
-    $('.columnTexts__btn--hideAbout').click(function() {
-        $('.columnTexts__moreInfo--hideAbout').slideToggle("slow");
-    });
+  $('.columnTexts__btn--hideAbout').click(function () {
+    $('.columnTexts__moreInfo--hideAbout').slideToggle('slow');
+  });
 });
 
 jQuery(document).ready(($) => {
-    "use strict";
-    $('#customers-testimonials').owlCarousel({
-        loop: true,
-        center: true,
-        items: 3,
-        margin: 0,
-        autoplay: true,
-        dots: true,
-        autoplayTimeout: 4500,
-        smartSpeed: 450,
-        responsive: {
-            0: {
-                items: 1
-            },
-            768: {
-                items: 2
-            },
-            1170: {
-                items: 3
-            }
-        }
-    });
+  'use strict';
+  $('#customers-testimonials').owlCarousel({
+    loop: true,
+    center: true,
+    items: 3,
+    margin: 0,
+    autoplay: true,
+    dots: true,
+    autoplayTimeout: 4500,
+    smartSpeed: 450,
+    responsive: {
+      0: {
+        items: 1
+      },
+      768: {
+        items: 2
+      },
+      1170: {
+        items: 3
+      }
+    }
+  });
 });
 
 const links = document.querySelectorAll('.nav__element');
 const sections = document.querySelectorAll('.wrapSection');
 
 function changeLinkState() {
-    let index = sections.length;
+  let index = sections.length;
 
-    while (--index && window.scrollY + 200 < sections[index].offsetTop) {}
+  while (--index && window.scrollY + 200 < sections[index].offsetTop) {}
 
-    links.forEach((link) => link.classList.remove('nav__element--selected'));
-    links[index].classList.add('nav__element--selected');
+  links.forEach((link) => link.classList.remove('nav__element--selected'));
+  links[index].classList.add('nav__element--selected');
 }
 
 changeLinkState();
 window.addEventListener('scroll', changeLinkState);
-
 
 // var lastScrollTop = 0;
 
@@ -165,114 +158,110 @@ const parallaxInstance = new Parallax(scene);
 const parallaxInstance3 = new Parallax(scene3);
 
 //send email
-$(".contact__button").click(function (e) {
-    e.preventDefault();
+$('.contact__button').click(function (e) {
+  e.preventDefault();
 
-    let contact__name = document.querySelector(".contact__name").value;
-    contact__name = DOMPurify.sanitize(contact__name);
+  let contact__name = document.querySelector('.contact__name').value;
+  contact__name = DOMPurify.sanitize(contact__name);
 
-    let contact__email = document.querySelector(".contact__email").value;
-    contact__email = DOMPurify.sanitize(contact__email);
+  let contact__email = document.querySelector('.contact__email').value;
+  contact__email = DOMPurify.sanitize(contact__email);
 
-    let contact__message = document.querySelector(".contact__message").value;
-    contact__message = DOMPurify.sanitize(contact__message);
+  let contact__message = document.querySelector('.contact__message').value;
+  contact__message = DOMPurify.sanitize(contact__message);
 
-    let contact__tel = document.querySelector(".contact__tel").value;
-    contact__message = DOMPurify.sanitize(contact__tel);
+  let contact__tel = document.querySelector('.contact__tel').value;
+  contact__message = DOMPurify.sanitize(contact__tel);
 
-    const contactInputs = document.querySelectorAll('.contact__input');
+  const contactInputs = document.querySelectorAll('.contact__input');
 
-    const clearErrorInfos = () => {
-        const errorInfos = document.querySelectorAll('.contact__errors');
-        errorInfos.forEach(element => {
-            element.remove();
-        });
-    }
+  const clearErrorInfos = () => {
+    const errorInfos = document.querySelectorAll('.contact__errors');
+    errorInfos.forEach((element) => {
+      element.remove();
+    });
+  };
 
-    let focusFlag = false;
+  let focusFlag = false;
 
-    const validateInputs = () => {
-        contactInputs.forEach(element => {
-            const errorInfo = document.createElement("p");
-            errorInfo.classList.add('contact__errors');
-            const email = $(".contact__email").val();
+  const validateInputs = () => {
+    contactInputs.forEach((element) => {
+      const errorInfo = document.createElement('p');
+      errorInfo.classList.add('contact__errors');
+      const email = $('.contact__email').val();
 
-            function validateEmail(email) {
-                let re = /\S+@\S+\.\S+/;
-                return re.test(email);
-            }
+      function validateEmail(email) {
+        let re = /\S+@\S+\.\S+/;
+        return re.test(email);
+      }
 
-            if (!element.value) {
-                if (focusFlag == false) {
-                    element.focus();
-                    focusFlag = true;
-                }
+      if (!element.value) {
+        if (focusFlag == false) {
+          element.focus();
+          focusFlag = true;
+        }
 
-                errorInfo.textContent = 'Pole jest wymagane.';
-                element.insertAdjacentHTML('afterEnd', errorInfo.outerHTML)
-                return false;
-            } else if (element.value && !validateEmail(email) && element.classList.contains('contact__input--email')) {
-                element.focus();
-                focusFlag = true;
-                errorInfo.textContent = 'Wpisz poprawny email.';
-                element.insertAdjacentHTML('afterEnd', errorInfo.outerHTML)
-                return false;
-            }
-
-        });
-    }
-
-    const clearInputsOnTyping = () => {
-        contactInputs.forEach(input => {
-            input.addEventListener('keydown', clearInput => {
-                input.nextSibling.textContent = '';
-            })
-        })
-    }
-
-    clearErrorInfos();
-    validateInputs();
-    clearInputsOnTyping();
-
-    if (focusFlag == true) {
+        errorInfo.textContent = 'Pole jest wymagane.';
+        element.insertAdjacentHTML('afterEnd', errorInfo.outerHTML);
         return false;
-    } else {
+      } else if (element.value && !validateEmail(email) && element.classList.contains('contact__input--email')) {
+        element.focus();
+        focusFlag = true;
+        errorInfo.textContent = 'Wpisz poprawny email.';
+        element.insertAdjacentHTML('afterEnd', errorInfo.outerHTML);
+        return false;
+      }
+    });
+  };
 
-        $.ajax({
-            type: "POST",
-            contentType: "application/x-www-form-urlencoded; charset=iso-8859-1",
-            url: "gmail.php",
+  const clearInputsOnTyping = () => {
+    contactInputs.forEach((input) => {
+      input.addEventListener('keydown', (clearInput) => {
+        input.nextSibling.textContent = '';
+      });
+    });
+  };
 
-            data: {
-                contact__email: contact__email,
-                contact__name: contact__name,
-                contact__message: contact__message,
-                contact__tel: contact__tel
-            },
+  clearErrorInfos();
+  validateInputs();
+  clearInputsOnTyping();
 
-            success: function (result) {
+  if (focusFlag == true) {
+    return false;
+  } else {
+    $.ajax({
+      type: 'POST',
+      contentType: 'application/x-www-form-urlencoded; charset=iso-8859-1',
+      url: 'gmail.php',
 
-                console.log(result);
-                const errorInfo = document.createElement("p");
+      data: {
+        contact__email: contact__email,
+        contact__name: contact__name,
+        contact__message: contact__message,
+        contact__tel: contact__tel
+      },
 
-                if (result.replace(/\s/g, '') == "ok") {
-                    document.querySelector(".contact__email").value = '';
-                    document.querySelector(".contact__message").value = '';
-                    document.querySelector(".contact__name").value = '';
-                    document.querySelector(".contact__tel").value = '';
+      success: function (result) {
+        console.log(result);
+        const errorInfo = document.createElement('p');
 
-                    errorInfo.textContent = 'Wiadomość wysłano pomyślnie.';
-                    errorInfo.classList.add('contact__errors', 'contact__errors--success');
-                    document.querySelector(".contact__message").insertAdjacentHTML('afterEnd', errorInfo.outerHTML);
-                } else if (result.replace(/\s/g, '') !== "ok") {
-                    errorInfo.textContent = 'Błąd, nie udało się wysłać wiadomości.';
-                    errorInfo.classList.add('contact__errors');
-                    document.querySelector(".contact__message").insertAdjacentHTML('afterEnd', errorInfo.outerHTML);
-                }
-            }
-        });
-    }
+        if (result.replace(/\s/g, '') == 'ok') {
+          document.querySelector('.contact__email').value = '';
+          document.querySelector('.contact__message').value = '';
+          document.querySelector('.contact__name').value = '';
+          document.querySelector('.contact__tel').value = '';
+
+          errorInfo.textContent = 'Wiadomość wysłano pomyślnie.';
+          errorInfo.classList.add('contact__errors', 'contact__errors--success');
+          document.querySelector('.contact__message').insertAdjacentHTML('afterEnd', errorInfo.outerHTML);
+        } else if (result.replace(/\s/g, '') !== 'ok') {
+          errorInfo.textContent = 'Błąd, nie udało się wysłać wiadomości.';
+          errorInfo.classList.add('contact__errors');
+          document.querySelector('.contact__message').insertAdjacentHTML('afterEnd', errorInfo.outerHTML);
+        }
+      }
+    });
+  }
 });
-
 
 // mocha.run();
